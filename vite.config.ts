80f02ff1694865d68402794e7d0e5e533b7248a6
Felipe Gadea Llopis <felipe@fgadea.dev>
2025-04-27 10:43:56 +0000

    more changes

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/fgadea-portfolio/",
+  base: "/fgadea-portfolio",
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
