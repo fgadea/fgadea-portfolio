4038d3831982990b76e62e307f5ab66b6bc526de
FELIPE GADEA LLOPIS <felipegadeallopis@FELIPEs-MacBook-Pro.local>
2025-05-03 21:55:40 +0300

    remove warning

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { Menu, X, Moon, Sun, Smartphone } from 'lucide-react';
+import { Menu, X, Moon, Sun } from 'lucide-react';
 import { useTheme } from '../contexts/ThemeContext';
 
 interface HeaderProps {
